419164881c01f68d70a1a4627bb166812e03ab37
Mário Sedlák <156649833+mase-sk@users.noreply.github.com>
2024-05-27 21:08:30 +0200

    Initial Commit (Basic Structure)
    Basic structure for this project.
    This is just a initial commit to main.

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+/*
+    Author: Mario Sedlak
+    Web:    mase.sk
+    Github: https://github.com/mase-sk
+    Date:   27.5.2024
+    Ver:    0_init
+    
+    License: You are not allowed to use this design and code
+             or any part of it in your own project!
+*/
